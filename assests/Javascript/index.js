766d9a807ba9eaec10599065c325596f4aea942a
Marymuriga <marymuriga@gmail.com>
2022-06-22 17:50:14 +0300

    GET  data from API

---
+++ assests/Javascript/index.js
@@ -0,0 +1,11 @@
+const getMealBtn = document.getElementById('get_meal');
+const mealContainer = document.getElementById('meal');
+
+
+getMealBtn.addEventListener('click', () =>{
+    fetch('www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => { createMeal(res.meals[0])})
+     
+});
+
