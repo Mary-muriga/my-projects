3507f3e2f36b46b7c89f288211bab9e2ea58f297
Marymuriga <marymuriga@gmail.com>
2022-06-23 11:23:46 +0300

    modifying my Js  function

--- assests/Javascript/index.js
+++ assests/Javascript/index.js
@@ -14,22 +14,44 @@ getMealBtn.addEventListener('click', () =>{
      
 });
 function renderMeal(mealObj){
+    const ingredients = [];
+    for(i=1; i<=20; i++){
+        if(mealObj[`strIngredients${i}`]){
+            ingredients.push(
+                `${mealObj[`strIngredient${i}`]}-
+            ${mealObj[`strMeasure${i}`]}`
+            )
+        }else {
+            break;
+        }
+    }
+
+    console.log(ingredients);
+
       let meals = mealObj.meals[0].strMeal
+      let category = mealObj.meals[0].strCategory
       let imgUrl = mealObj.meals[0].strMealThumb
-      let par= mealObj.meals[0].strInstructions
-      let lst = mealObj.meals[0].strIngredient1
-      let lst1  = mealObj.meals[0].strIngredient2
+      let par = mealObj.meals[0].strInstructions
+      //let ingredients = mealObj.meals[0].strIngredient1
+      //let ingredients1 = mealObj.meals[0].strIngredient2
+      //let ingredients2 = mealObj.meals[0].strIngredient3
+      //let ingredients3 = mealObj.meals[0].strIngredient4
+      //let ingredients4 = mealObj.meals[0].strIngredient5
+      //let ingredients5 = mealObj.meals[0].strIngredient6
+      //let ingredients6 = mealObj.meals[0].strIngredient7
+      //let ingredients7 = mealObj.meals[0].strIngredient8
       console.log(meals)
       let mealDiv = document.createElement('div')
       mealDiv.setAttribute("id", "meal-display")
       mealDiv.innerHTML = `<img src = "${imgUrl}"/>
                         <h3>${meals}</h3>
                         <h3> ingredients</h3>
-                        <p>${lst}</p>
-                        <p>${lst1}</p>
-                        <p></p>
-                        <p></p>
+                        <p>${ingredients}</p>
+                    
+                        <h3> Instructions </h3>
                         <p> ${par}</p>`
+                        
+                               
                     
     mealDisplay.append(mealDiv)
     console.log(mealDiv)
